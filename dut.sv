31894fc0ada5708d0589d80aaf9d225602338216
Eldon Nelson <eldon_nelson@ieee.org>
2015-07-19 19:42:22 -0500

    simplier names

--- dut.sv
+++ dut.sv
@@ -1,9 +1,8 @@
-module multiplier (
+module mul (
                    output [31:0] mult,
                    input [15:0] mop1, data
                    );
-    // RTL code for the multiplier output register
-endmodule // multiplier
+endmodule // mul
 
 module dut (
               input [15:0] data
@@ -14,18 +13,18 @@ module dut (
 `include "uvm_macros.svh"
     import uvm_pkg::*;
 
-    // Example 1 : the .name implicit port connection
-    multiplier multiplier_ex1 (.mult, .mop1, .data);
+    // Example 4 : the .* implicit port connection
+    mul ex4 (.*);
+    
+    // Example 3 : the .name implicit port connection
+    mul ex3 (.mult, .mop1, .data);
 
     // Example 2 : the .name implicit port connection - unconnect one
-    multiplier multiplier_ex2 (.mult, .mop1);
+    mul ex2 (.mult, .mop1);
 
-    // Example 3 : the .name implicit port connection - intentionally unconnect
-    multiplier multiplier_ex3 (.mult, .mop1, .data());
-    
-    // Example 4 : the .* implicit port connection
-    multiplier multiplier_ex4 (.*);
-       
+    // Example 1 : the .name implicit port connection - intentionally unconnect
+    mul ex1 (.mult, .mop1, .data());
+           
     initial begin
         `uvm_info("HELP", "START", UVM_MEDIUM);
         $finish();
